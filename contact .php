4a41dc59700ba3377ab8be65468a6df5b87ba1f8
rajavel452 <60314027+rajavel452@users.noreply.github.com>
2020-02-01 10:38:43 +0530

    Update contact .php

--- contact .php
+++ contact .php
@@ -4,18 +4,18 @@
 	 $message = $_POST['message'];
 	 
 	 
-	 $email_from = 'https://rajavel452.github.io/';
+	 $email_from = 'https://rajavel452.github.io';
 	 
 	 $email_subject = " contact form";
-	 $email_body = "User Name: $name.\n"
+	 $email_body = "User Name: $name.\n".
 	                "User Email:$visitor_email.\n".
-					  "User Message:$messaage.\n":
+					  "User Message:$messaage.\n";
 	 $to = "rajavel452@gmail.com";
 
      $headers = "From: $email_from\r\n";
-     $headers = "Reply-To :$visitor_email \r\n";
-	 mail($to,$email_subject,$email_body,headers);
-	 header("Location: index.html");
+     $headers .= "Reply-To :$visitor_email \r\n";
+     mail($to,$email_subject,$email_body,$headers);
+     header("Location: index.html");
 	 
 	 
  ?>
